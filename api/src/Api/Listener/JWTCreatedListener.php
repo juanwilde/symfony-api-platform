2bdddb17789d4a5e486ae43610bb953abb3d0395
Juan Gonzalez <juangonzalezmontes@gmail.com>
2020-07-23 16:47:19 +0200

    [section11] - Logic action logic created

---
+++ api/src/Api/Listener/JWTCreatedListener.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Api\Listener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        /** @var User $user */
+        $user = $event->getUser();
+
+        $payload = $event->getData();
+        $payload['id'] = $user->getId();
+        unset($payload['roles']);
+
+        $event->setData($payload);
+    }
+}
